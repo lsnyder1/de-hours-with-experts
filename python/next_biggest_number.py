d160e109022a453c0c21ba6d1555a2aeecb8ff82
Lonny Snyder <lonnysnyder81@gmail.com>
2021-02-16 18:54:25 -0600

    capturing missed file

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
+import classes
 
 def main():
-    next_biggest_number(sys.argv[1])
+    print (next_biggest_number(sys.argv[1]))
 
 
 def next_biggest_number(num):
     #TODO: Implement me!
-    return 0
+    numberObj=classes.NumberObj(num)
 
+    #return numberObj.getNextBiggestPos()
+    return numberObj.dump()
 if __name__ == "__main__":
     main()
-
-
-
