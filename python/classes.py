80a30d147c4a9f0b2282d515c70a2ea3f83d77e1
Lonny Snyder <lonnysnyder81@gmail.com>
2021-02-17 14:22:15 -0600

    refactoring, removed print statements and replaced while loop with for x in range()

--- python/classes.py
+++ python/classes.py
@@ -14,9 +14,6 @@ class NumberObj(object):
 
 
 
-
-
-
     def getNextBiggest(self):
         #cast the string to int list
         numberList=[int(x) for x in self.processedInputStr]
@@ -24,58 +21,46 @@ class NumberObj(object):
         finishedStrList=[]
         #holds all of the numbers we will remove from numberList, we will have to reorganize these to build correct number
         tempList=[]
-        #max index for numberList to use as start for while loop counter to work from right ot left
-        i=-1
+
+        #must be more than one number if we are to attempt to rearrange
         if self.processedInputLength>1:
-            i=self.processedInputLength-1
 
-        while i > -1:
-            #print(f"numList: {numberList}")
-            #Break from the while loop by returning as soon as there is a value higher than the last remaining index of numberList, or lower if negative
-            #hold this value because we are going to remove it from numberList and need to compare in loop below
-            tempVal=numberList[i]
-            #moving values between lists to indicate
-            tempList.append(tempVal)
-            tempList.sort()
-            del numberList[i]
+            for i in range(self.processedInputLength-1,0,-1):
+                #hold this value because we are going to remove it from numberList and need to compare in loop below
+                tempVal=numberList[i]
+                tempList.append(tempVal)
+                tempList.sort()
+                del numberList[i]
 
-            #testLogs to be removed
-            #print(f"tempVal: {tempVal}")
-            #print(f"maxTempList: {max(tempList)}")
-            #print(f"TempList: {tempList}")
 
             #if this evaulates to true we are done looping, time to build the return string
-            if(max(tempList)>tempVal and not self.isNegative) or (min(tempList)<tempVal and self.isNegative):
-                if self.isNegative==False:
-                #positive numbers
+                if(max(tempList)>tempVal and not self.isNegative) or (min(tempList)<tempVal and self.isNegative):
+                    if self.isNegative==False:
+                        #positive numbers
+                        biggerThanIList=[x for x in tempList if x>tempVal]
+                        biggerThanIList.sort()
+                        #put the lowest value back in numberList
+                        numberList.append(biggerThanIList[0])
+                        #remove this number from tempList, don't care if more than one instance as long as we remove one
+                        tempList.remove(biggerThanIList[0])
+                        #sort to ensure ascending order
+                        tempList.sort()
 
-                    biggerThanIList=[x for x in tempList if x>tempVal]
-                    biggerThanIList.sort()
-                    #put the lowest value back in numberList
-                    numberList.append(biggerThanIList[0])
-                    #remove this number from tempList, don't care if more than one instance as long as we remove one
-                    tempList.remove(biggerThanIList[0])
-                    #sort to ensure ascending order
-                    tempList.sort()
+                    else:
+                        #negative numbers just the reverse of above with the exception of adding the"-" to the output List
+                        smallerThanIList=[x for x in tempList if x<tempVal]
+                        smallerThanIList.sort(reverse=True)
+                        numberList.append(smallerThanIList[0])
+                        tempList.remove(smallerThanIList[0])
+                        tempList.sort(reverse=True)
+                        finishedStrList.insert(0,"-")
 
-                else:
-                #negative numbers
+                    #+=here since we already have [0] if negative
+                    finishedStrList+=[str(y) for y in numberList+tempList]
+                    if self.isFloat:
+                        finishedStrList.insert(self.numberStr.index("."),".")
+                    return "".join(finishedStrList)
 
-                    smallerThanIList=[x for x in tempList if x<tempVal]
-                    smallerThanIList.sort(reverse=True)
-                    numberList.append(smallerThanIList[0])
-                    tempList.remove(smallerThanIList[0])
-                    tempList.sort(reverse=True)
-                    finishedStrList.insert(0,"-")
-
-                #+=here since we already have [0] if negative
-                finishedStrList+=[str(y) for y in numberList+tempList]
-                if self.isFloat:
-                    finishedStrList.insert(self.numberStr.index("."),".")
-                return "".join(finishedStrList)
-
-            #decrementing as working right to left through user numberList
-            i-=1
             #if we ever get here, no higher number was possible
         return -1
 
--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -8,9 +8,9 @@ def main():
 
 def next_biggest_number(num):
     #TODO: Implement me!
+    #went a little ham here and used a class. I'm a little rusty with python so I had a little fun with this.
     numberObj=classes.NumberObj(num)
-
     return numberObj.getNextBiggest()
-    #return numberObj.getNextBiggest()
+
 if __name__ == "__main__":
     main()
